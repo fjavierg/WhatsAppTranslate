8f8a0ffaf6701df161db0246295f788c3f3c9860
Javier <Javier@Main-Laptop>
2016-01-16 11:08:49 +0100

    Use wa events system (MyEvents.php)

---
+++ WhatsAppTranslate/application/MyEvents.php
@@ -0,0 +1,105 @@
+<?php
+
+require 'AllEvents.php';
+
+class MyEvents extends AllEvents
+{
+    /**
+     * This is a list of all current events. Uncomment the ones you wish to listen to.
+     * Every event that is uncommented - should then have a function below.
+     *
+     * @var array
+     */
+    public $activeEvents = [
+//        'onClose',
+//        'onCodeRegister',
+//        'onCodeRegisterFailed',
+//        'onCodeRequest',
+//        'onCodeRequestFailed',
+//        'onCodeRequestFailedTooRecent',
+        'onConnect',
+//        'onConnectError',
+//        'onCredentialsBad',
+//        'onCredentialsGood',
+        'onDisconnect',
+//        'onDissectPhone',
+//        'onDissectPhoneFailed',
+//        'onGetAudio',
+//        'onGetBroadcastLists',
+//        'onGetError',
+//        'onGetExtendAccount',
+//        'onGetGroupMessage',
+//        'onGetGroupParticipants',
+//        'onGetGroups',
+//        'onGetGroupsInfo',
+//        'onGetGroupsSubject',
+//        'onGetImage',
+//        'onGetLocation',
+        'onGetMessage',
+//        'onGetNormalizedJid',
+//        'onGetPrivacyBlockedList',
+//        'onGetProfilePicture',
+//        'onGetReceipt',
+//        'onGetServerProperties',
+//        'onGetServicePricing',
+//        'onGetStatus',
+//        'onGetSyncResult',
+//        'onGetVideo',
+//        'onGetvCard',
+//        'onGroupCreate',
+//        'onGroupisCreated',
+//        'onGroupsChatCreate',
+//        'onGroupsChatEnd',
+//        'onGroupsParticipantsAdd',
+//        'onGroupsParticipantsPromote',
+//        'onGroupsParticipantsRemove',
+//        'onLoginFailed',
+//        'onLoginSuccess',
+//        'onAccountExpired',
+//        'onMediaMessageSent',
+//        'onMediaUploadFailed',
+//        'onMessageComposing',
+//        'onMessagePaused',
+//        'onMessageReceivedClient',
+//        'onMessageReceivedServer',
+//        'onPaidAccount',
+//        'onPing',
+//        'onPresenceAvailable',
+//        'onPresenceUnavailable',
+//        'onProfilePictureChanged',
+//        'onProfilePictureDeleted',
+//        'onSendMessage',
+//        'onSendMessageReceived',
+//        'onSendPong',
+//        'onSendPresence',
+//        'onSendStatusUpdate',
+//        'onStreamError',
+//        'onUploadFile',
+//        'onUploadFileFailed',
+    ];
+    protected $translator;
+    protected $toLanguage = "en";
+    
+    public function setTranslator($translator)
+    {
+    	$this->translator = $translator;
+    }
+
+    public function onConnect($mynumber, $socket)
+    {
+        echo "Phone number $mynumber connected successfully!";
+    }
+
+    public function onDisconnect($mynumber, $socket)
+    {
+        echo "Phone number $mynumber is disconnected!";
+    }
+    public function onGetMessage( $mynumber, $from, $id, $type, $time, $name, $body )
+    {
+    	$tmp=explode("@",$from);
+    	$fromNb = $tmp[0];
+    	echo "Message from $fromNb:$body\n\n";
+    	$translatedBody = $this->translator->translate($body,$this->toLanguage);
+    	$this->whatsProt->sendMessage($fromNb,$translatedBody );
+    }
+}
